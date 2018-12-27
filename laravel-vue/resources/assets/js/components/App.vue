8f0e50357ae0780ef02bfface610b4b9c415c3eb
aljajazva <gotsulskaya@gmail.com>
2018-12-27 15:25:56 +0200

    symple vue app

---
+++ laravel-vue/resources/assets/js/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>It's my first vui application</p>
+        {{ message }}
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello World'
+            }
+        }
+    }
+</script>
+<style scoped>
+    div {
+        text-align: center;
+    }
+</style>
